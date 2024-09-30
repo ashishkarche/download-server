d409ac09737200320a31f3ab0c4a31632fe8559b
ashishkarche <ashishkarche9@gmail.com>
2024-09-30 18:55:00 +0530

    updated

--- src/DownloadPage.js
+++ src/DownloadPage.js
@@ -16,11 +16,11 @@ const DownloadPage = () => {
     if (tokenFromUrl) {
       setToken(tokenFromUrl);
       axios
-        .post('https://server-coral-kappa-23.vercel.app/check-token', { token: tokenFromUrl })
+        .post('https://server-two-self-13.vercel.app/check-token', { token: tokenFromUrl })
         .then((response) => {
           if (response.data.success) {
             setIsValidToken(true);
-            window.location.href = `https://server-coral-kappa-23.vercel.app/download?token=${tokenFromUrl}`;
+            window.location.href = `https://server-two-self-13.vercel.app/download?token=${tokenFromUrl}`;
           } else {
             setIsValidToken(false);
           }
